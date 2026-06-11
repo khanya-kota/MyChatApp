7daa42145e2265def8a587af4e4b6cc1b3e9a4c4
khanya-kota <st10512866@rcconnect.edu.za>
2026-06-11 10:53:12 +0200

    feat : Added LoginTest and MessageTest - all unit tests passing
    Added part 3 in message test and LoginTest

--- src/test/java/MessageTest.java
+++ src/test/java/MessageTest.java
@@ -3,10 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 import com.mycompany.mychatapp.Message;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
@@ -19,43 +15,46 @@ public class MessageTest {
     
     @Test
     public void testMessageCreation() {
-        Message msg = new Message("Hello World");
-        assertNotNull(msg.getMessageId(), "Message ID should not be null");
-        assertNotNull(msg.getHashCode(), "Hash should not be null");
+        Message msg = new Message("+27712345678", "Hello World");
+        assertNotNull(msg.getMessageID(), "Message ID should not be null");
+        assertNotNull(msg.getMessageHash(), "Hash should not be null");
         assertEquals("Hello World", msg.getMessageText(), 
                      "Message text should match");
     }
     
     @Test
     public void testUniqueIds() {
-        Message msg1 = new Message("Test1");
-        Message msg2 = new Message("Test2");
+        Message msg1 = new Message("+27712345678", "Test1");
+        Message msg2 = new Message("+27712345678", "Test2");
         
-        assertNotEquals(msg1.getMessageId(), msg2.getMessageId(), 
+        assertNotEquals(msg1.getMessageID(), msg2.getMessageID(), 
                         "Each message should have unique ID");
     }
     
     @Test
     public void testHashGeneration() {
-        Message msg = new Message("Test");
-        assertEquals(2, msg.getHashCode().length(), 
-                     "Hash should be 2 characters");
-        assertEquals(msg.getMessageId().substring(0, 2), msg.getHashCode(), 
-                     "Hash should match first 2 chars of ID");
+        Message msg = new Message("+27712345678", "Hello World");
+        String[] hashParts = msg.getMessageHash().split(":");
+
+        assertEquals(3, hashParts.length, "Hash should have 3 parts");
+        assertEquals(msg.getMessageID().substring(0, 2), hashParts[1],
+                     "Hash should include first 2 chars of ID");
+        assertEquals("HELLOWORLD", hashParts[2],
+                     "Hash should end with first and last words in uppercase");
     }
     
     @Test
     public void testStoreMessage() {
-        Message msg = new Message("Test message");
+        Message msg = new Message("+27712345678", "Test message");
         String testFile = "test_message.json";
-        
-        msg.storeMessage(testFile);
-        
         File file = new File(testFile);
-        assertTrue(file.exists(), "JSON file should be created");
         
-        // Clean up
-        file.delete();
+        try {
+            msg.storeMessage(testFile);
+            assertTrue(file.exists(), "JSON file should be created");
+        } finally {
+            file.delete();
+        }
     }
 }
 
